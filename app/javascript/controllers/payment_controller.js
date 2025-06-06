8827604ad91ea739129016e15c5ebb944c2048ba
CristinaLazar12 <cristinalazar122@gmail.com>
2025-06-06 11:32:54 +0300

    Pay Type JS Functionality + tests

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -9,11 +9,16 @@ export default class extends Controller {
   }
 
   showAdditionalFields() {
+
     let selection = this.selectionTarget.value
 
     for (let fields of this.additionalFieldsTargets) {
       fields.disabled = fields.hidden = (fields.dataset.type != selection)
     }
+
+    console.log(selection)
+    console.log(this.additionalFieldsTargets)
+
   }
 }
 
